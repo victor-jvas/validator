06b0a9fc2e9c04337dc306917e62fee9f784de38
Joao Victor <victor.jvas1@gmail.com>
2023-09-20 16:26:06 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func main() {
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen to server", err)
+	}
+}
+
+func basicHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Hello, World"))
+}
