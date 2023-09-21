96ffdd63c90e91851eb0eda949bf3e3ee43858a7
Joao Victor <victor.jvas1@gmail.com>
2023-09-21 17:29:31 -0300

    added routing

--- application/routes.go
+++ application/routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+	"validator/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -15,5 +16,18 @@ func loadRoutes() *chi.Mux {
 		writer.WriteHeader(http.StatusOK)
 	})
 
+	router.Route("/order", LoadOrderRoutes)
+
 	return router
+}
+
+func LoadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
+
 }
